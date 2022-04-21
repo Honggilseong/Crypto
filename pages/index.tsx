fcc50367d3153ab1049d51487f80a49ea7166799
unknown <hong.gilseong@gmail.com>
2022-04-21 19:25:36 +0900

    remove data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 import Home from '../components/Home'
 
-const App: NextPage = ({ data }: any) => {
-  return <Home data={data} />
+const App: NextPage = () => {
+  return <Home />
 }
 
 export default App
